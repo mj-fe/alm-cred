4fec77d4f4556cac93ef0f0f2692d3e9f280ea93
mj-fe <mj.fe.developer@gmail.com>
2024-10-23 18:53:04 -0300

    fix: structure for github pages

--- pwa-sw.js
+++ pwa-sw.js
@@ -12,7 +12,7 @@ var offline_page = 'offline.html';
 var first_cache_urls = [start_page, offline_page];
 var never_cache_urls = [/\/private.html/, /\/panel/, /\/custom-url/];
 
-// Install 
+// Install
 self.addEventListener('install', function (e) {
 	console.log('PWA sw installation');
 	e.waitUntil(caches.open(cache_storage_name).then(function (cache) {
@@ -72,7 +72,7 @@ self.addEventListener('fetch', function (e) {
 	}));
 });
 
-// Check never cache urls 
+// Check never cache urls
 function check_never_cache_urls(url) {
 	if (this.match(url)) {
 		return false;
