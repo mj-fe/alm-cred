99e952ab363ae2b997e77747877452cf364ebdec
mj-fe <mj.fe.developer@gmail.com>
2024-10-28 18:49:47 -0300

    feat: onboarding stepper

--- js/layout.js
+++ js/layout.js
@@ -1,8 +1,39 @@
+const KEYS = {
+    token: 'token'
+}
+
 $(() => {
-    $('#header').load('layout/navbar.html')
+    const token = localStorage.getItem(KEYS.token)
+
+    $('#header').load('layout/navbar.html', () => {
+        const backTo = $('#header').attr('back-to')
+        const isLoggedIn = Boolean(JSON.parse(token))
+
+        $('#logo-link').attr('href', isLoggedIn ? 'home.html' : '/')
+
+        if (backTo) {
+            $('.sidebar-toggler').addClass('d-none')
+            $('#back-btn').attr('href', backTo).removeClass('d-none')
+        }
+    })
+
     $('#sidebar').load('layout/sidebar.html')
 
     $('body').on('click', '.sidebar-toggler', (e) => {
         $('#sidebar').toggleClass('hidden')
     })
+
+    $('body').on('click', '#fake-login', (e) => {
+        window.localStorage.setItem(KEYS.token, JSON.stringify(fakeToken()))
+        window.location.href = `${window.location.origin}/home.html`
+    })
 })
+
+function fakeToken(length = 16) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const token = Array.from({ length }, () => {
+        return chars.charAt(Math.floor(Math.random() * chars.length));
+    })
+
+    return token.join('');
+}
---
+++ js/masks.js
@@ -0,0 +1,11 @@
+$(() => {
+    $('.code-mask').mask('0')
+    $('.cep-mask').mask('00000-000')
+    $('.date-mask').mask("00/00/0000")
+    $('.ddd-mask').mask("00")
+    $('.phone-mask').mask("00000-0000")
+    $('.phone-with-ddd-mask').mask("(00) 00000-0000'")
+    $('.money-mask').mask("#.##0,00", { reverse: true })
+    $('.cpf-mask').mask("000.000.000-00", { reverse: true })
+    $('.cnpj-mask').mask('00.000.000/0000-00', { reverse: true })
+})
