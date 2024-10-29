011844e318de937ae32c2b0297a3cc4d82dd5a9f
mj-fe <mj.fe.developer@gmail.com>
2024-10-29 17:40:29 -0300

    feat: new address

--- js/masks.js
+++ js/masks.js
@@ -1,11 +1,22 @@
 $(() => {
     $('.code-mask').mask('0')
-    $('.cep-mask').mask('00000-000')
-    $('.date-mask').mask("00/00/0000")
     $('.ddd-mask').mask("00")
+    $('.date-mask').mask("00/00/0000")
     $('.phone-mask').mask("00000-0000")
+    $('.zipcode-mask').mask('00000-000')
     $('.phone-with-ddd-mask').mask("(00) 00000-0000'")
     $('.money-mask').mask("#.##0,00", { reverse: true })
     $('.cpf-mask').mask("000.000.000-00", { reverse: true })
     $('.cnpj-mask').mask('00.000.000/0000-00', { reverse: true })
+    $('.state-mask').mask('AA', {
+        translation: {
+            'A': {
+                pattern: /[A-Za-z]/,
+                optional: false
+            },
+        },
+        onKeyPress: function(value, e, field, options) {
+            field.val(value.toUpperCase());
+        }
+    });
 })
